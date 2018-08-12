42a5c61fc0a714b9b3a3bf08b6649d6c0cca27d6
Caleb Huitt <development-caleb@huitt.org>
2018-08-12 15:24:15 -0500

    Allow targeting users from a file

--- notification.py
+++ notification.py
@@ -29,20 +29,29 @@ class Options(argparse.ArgumentParser):
 
         self.add_argument("--users", "--user", nargs='+', metavar="USER",
                           help="Notify the given user(s)")
+        self.add_argument("--user_list", metavar="FILE",
+                          help="Notify the user(s) given in the file (one per line)")
 
     def store_args(self):
         self.parsed_args = self.parse_args()
         self._compile_lists()
         if not self.usernames:
-            self.error("At least one user or user-file is required.")
+            self.error("At least one user or file of users is required.")
         self._normalize_usernames()
 
     def _compile_lists(self):
         self._add_command_line_users()
+        self._add_users_from_file()
 
     def _add_command_line_users(self):
         if self.parsed_args.users:
             self.usernames.extend(self.parsed_args.users)
+
+    def _add_users_from_file(self):
+        if self.parsed_args.user_list:
+            with open(self.parsed_args.user_list, 'r') as f:
+                for line in f:
+                    self.usernames.append(line.rstrip('\n') )
 
     def _normalize_usernames(self):
         normalized = set()
